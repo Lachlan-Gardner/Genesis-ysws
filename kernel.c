7ab1deffd635af8f6aff9f83bbd04b74a119934b
Lachlan Gardner <glachlan365@gmail.com>
2026-07-06 10:08:29 +1200

    Attempt at keyboard input and init.

--- kernel.c
+++ kernel.c
@@ -13,7 +13,13 @@
 // The location of the vga buffer in memory.
 // So it can be written to in order to draw things on the screen.
 // Is its position fixed?
-volatile uint16_t* vga_buffer = (uint16_t*) 0xB8000;
+volatile uint16_t *vga_buffer = (uint16_t*) 0xB8000;
+
+// The ports of the ps/2 controller for the keyboard.
+/// The prt for the statux and c    ommand registers.
+const uint8_t keyboard_status_command_register = (uint8_t) 0x60;
+/// The data port for the keyboard dara port.
+const uint8_t keyboard_data = (uint8_t) 0x64;
 
 // The vga has 80x25 characters by default.
 // Can it be increased?
@@ -27,8 +33,9 @@ int term_row = 0;
 // Makes the colour black background, white text.
 // The vga has sixteen colours, and we're setting the foreground and background using this.
 // 0 for black, F for white.
-uint8_t term_colour = 0x0F;
+uint8_t term_colour = 0x0D;
 
+/// @brief Initialises the terminal by writing every character as a space.
 void term_init()
 {
     // Iterates through every character in the vga buffer.
@@ -49,8 +56,8 @@ void term_init()
     }
 }
 
-// Add a character to the screen.
-// Is it bad practice to call a char "character"?
+/// @brief Puts a character on the screen.
+/// @param character The character to add. Is it bad practice to call a char character?
 void term_put_character(char character) 
 {
     switch(character) 
@@ -101,6 +108,8 @@ void term_put_character(char character)
     }
 }
 
+/// @brief Print a string to the terminal.
+/// @param str The string to print.
 void print_to_term(const char *str) 
 {
     // '/0' is the null character, which is what you would get at the end of the string.
@@ -112,6 +121,110 @@ void print_to_term(const char *str)
     }
 }
 
+/// @brief Reads a byte of data from a port.
+/// @param port Which port to read from.
+/// @return The byte.
+uint8_t read_port(uint16_t port) 
+{
+    uint8_t data;
+    __asm__ volatile ("inb %1, %0" : "=a"(data) : "Nd"(port));
+    return data;
+}
+
+/// @brief Write a byte to a port.
+/// @param port The port to write to.
+/// @param data_to_write what to write.
+void write_port(uint16_t port, uint8_t data_to_write)
+{
+    __asm__ volatile ("outb %0, %1" : : "a"(data_to_write), "Nd"(port));
+}
+
+
+char get_input()
+{
+    int pressed_enter = 0;
+    
+    print_to_term("\nStarting get input.     ");
+    
+    while (1) 
+    {
+        if (read_port(keyboard_status_command_register) & 1) 
+        {
+            if (read_port(keyboard_data) == 0x02)
+            {
+                return '1';
+            }
+        }
+    }
+}
+
+int ps_port_init()
+{
+    // Disable the first port.
+    write_port(keyboard_status_command_register, 0xAD);
+    // Disable the sceond port.
+    write_port(keyboard_status_command_register, 0xAD);
+    
+    read_port(keyboard_data);
+    
+    // Flushing the bits of the ps/2 controller.
+    
+    /// Disable IRQ.
+    uint8_t flush_bit_0 = 1;
+    /// Disables translation of port 1.
+    uint8_t flush_bit_6 = 1 << 6;
+    /// Enables clock signal.
+    uint8_t flush_bit_4 = 1 << 4;
+    
+    // I feel so smart rn.
+    uint8_t total_flush = ~(0 | flush_bit_0 | flush_bit_6 | flush_bit_4);
+    
+    uint8_t flushed_port = read_port(keyboard_status_command_register) & total_flush;
+    
+    write_port(keyboard_status_command_register, flushed_port);
+    
+    write_port(keyboard_status_command_register, 0xAA);
+    
+    while (!(read_port(keyboard_status_command_register) & 1)) 
+    {
+        // Waits until results of the are ready.
+        //! If there's an infite loop it's probably this.
+    }
+    
+    const uint8_t result_from_self_test = read_port(keyboard_status_command_register);
+    
+    // Test failed.
+    if (result_from_self_test != 0x55)
+    {
+        // "And now we know."
+        print_to_term("\nPs/2 Controller failed self test.");
+        
+        // Reflects the failure.
+        return 0;
+    }
+    
+    // Tries to enable the second port.
+    write_port(keyboard_status_command_register, 0xA8);
+    
+    write_port(keyboard_status_command_register, 0x20);
+    
+    while (!(read_port(keyboard_status_command_register) & 1)) 
+    {
+        // Waits until results of the are ready.
+        //! If there's an infite loop it's probably this.
+    }
+    
+    const uint8_t result_from_configuration_test = read_port(keyboard_status_command_register);
+    
+    if (result_from_configuration_test & (1 << 4))
+    {
+        print_to_term("\nOnly one port ps/2 controller.");
+    } else {
+        // Disable tbe second port.
+        write_port(keyboard_status_command_register, 0xA7);
+    } 
+}
+
 void kernel_main() 
 {
     // Clears the screen to get it ready.
@@ -119,6 +232,12 @@ void kernel_main()
     
     // Printing out some stuff.
     print_to_term("It works!\nEven on a new line!");
+    
+    char something = get_input();
+    
+    print_to_term("Finished getting input: ");
+    
+    term_put_character(something);
 }
 
 //TODO: Add variable colours.
